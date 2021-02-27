5f77e9ae37e6d81945adf04f074d09190146dc47
adalfa <adalfa@WIN8ADF>
2021-02-27 11:27:08 +0100

    Add project files.

---
+++ leet/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet
+{
+    public class Solution
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            int l = 0;
+
+            if ((pushed.Length != popped.Length)) return false;
+            if ((l = pushed.Length) == 0) return true;
+            if (l == 1) return pushed[0] == popped[0];
+
+            for (int i = 0; i < l - 2; i++)
+            {
+                Console.WriteLine("pushed:{0} popped:{1}", pushed[i], popped[l - 1 - i]);
+                if (pushed[i] != popped[l - 1 - i]) return false;
+
+            }
+            if (l > 2)
+                return (pushed[l - 2] == popped[0]) && (pushed[l - 1] == popped[1]);
+            else
+                return true;
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] results=new int[2];
+            if (nums.Length < 2) return new int[0];
+            for( int idx1=0;idx1<nums.Length;idx1++)
+            {
+                results[0] = idx1;
+                for(int idx2=0;idx2<nums.Length;idx2++)
+                {
+                    if (idx1!=idx2)
+                    {
+                        if (target-nums[idx2]==nums[idx1])
+                        {
+                            results[1] = idx2;
+                            return results;
+                        }
+                    }
+                }
+
+            }
+            return results;
+        }
+    }
+}
---
+++ leet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] pushed = new int[] { 1, 2, 3, 4, 5,6};
+            int[] popped = new int[] { 5,6, 4, 3, 2, 1 };
+            Console.WriteLine("uguale? {0}",s.ValidateStackSequences(pushed,popped));
+            Console.ReadKey();
+        }
+    }
+}
